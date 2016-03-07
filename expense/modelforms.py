875095de608b416c55932b28e8881e5e1d7a029c
Michael J. Ruschak <mike@debian>
2016-03-07 04:31:22 -0500

    Add common form-control class to each input form field.

--- expense/modelforms.py
+++ expense/modelforms.py
@@ -2,6 +2,14 @@ from django.forms import ModelForm
 from expense.models import Expense
 
 class ExpenseForm(ModelForm):
+	
 	class Meta:
 		model = Expense
 		fields = ['description', 'purchase_date', 'total', 'receipt']
+
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		for field in iter(self.fields):
+			self.fields[field].widget.attrs.update({
+				'class': 'form-control'
+			})
